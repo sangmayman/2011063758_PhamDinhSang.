76c77af4888f7f72b48501da1351f3f372d8fc98
sang <sang281102@gmail.com>
2024-03-26 16:09:53 +0700

    Dínhang_dev

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Đây là trang chủ</h1>
+    <p>Đây là nội dung trang chủ.</p>
+    <script src="js/script.js"></script>
+</body>
+</html>
